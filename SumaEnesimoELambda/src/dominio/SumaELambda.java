f676b9b520e94225c62308caf613141ac70e989c
Danii <danielfernandezlopez75@gmail.com>
2023-02-17 10:13:53 +0100

    aaa

---
+++ SumaEnesimoELambda/src/dominio/SumaELambda.java
@@ -0,0 +1,9 @@
+package dominio;
+
+public class SumaELambda {
+    // Suma enesima mediante expresiones lambda
+    public double sumaEnesima(int n) {
+        return (double) (1.0 / (n * (n + 1)));
+    }
+    
+}
