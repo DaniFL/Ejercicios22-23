cacb862cf259320fefae5ac7b969c7b3e1eeacc0
Danii <danielfernandezlopez75@gmail.com>
2023-03-06 15:25:22 +0100

    Ejercicio elevacion execepciones 2 (FACTORIAL)

--- Recursividad/FactorialR/src/aplicacion/Principal.java
+++ Recursividad/FactorialR/src/aplicacion/Principal.java
@@ -5,11 +5,23 @@ import dominio.Factorial;
 import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
-        Factorial f = new Factorial();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un numero: ");
-        double n = sc.nextInt();
-        sc.close();
-        System.out.println("El factorial de " + n + " es: " + f.calcularFactorial(n));
+        while (true) {
+        try {
+                Factorial f = new Factorial();
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Ingrese un numero: ");
+                int n = sc.nextInt();
+                sc.close();
+                System.out.println("El factorial de " + n + " es: " + f.calcularFactorial(n));
+                break;
+            }
+            catch (Exception e) {
+                System.out.println("ERROR:" + e.getMessage() + "\nPor favor, introduzca un numero entero positivo");
+            }
+        }
     }
 }
+
+
+
+
