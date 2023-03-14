18da694580656b47d70808c68896dfdf6faa0720
Danii <daniel.fernandezlopez@usp.ceu.es>
2023-03-14 20:00:58 +0100

    Desarrollo ejercicios lambda

--- ExpresionesLambda/DesviacionEX/src/aplicacion/Principal.java
+++ ExpresionesLambda/DesviacionEX/src/aplicacion/Principal.java
@@ -12,7 +12,17 @@ public class Principal {
             System.out.println("Ingrese el elemento " + (i + 1) + " de la lista: ");
             lista[i] = sc.nextInt();
         }
-        System.out.println();
+        sc.close();
+        double media = 0;
+        for (int i = 0; i < n; i++) {
+            media += lista[i];
+        }
+        media /= n;
+        double desviacion = 0;
+        for (int i = 0; i < n; i++) {
+            desviacion += Math.pow(lista[i] - media, 2);
+        }
+        desviacion = Math.sqrt(desviacion / n);
+        System.out.println("La desviación típica de la lista es: " + desviacion);
     }
-
 }
